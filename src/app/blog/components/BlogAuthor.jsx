145cfa4b053e1175fb3fae96bb1939e62d9f83b7
Raxxius <raxxman@gmail.com>
2023-05-10 22:24:06 +0100

    added icons

--- src/app/blog/components/BlogAuthor.jsx
+++ src/app/blog/components/BlogAuthor.jsx
@@ -1,16 +1,18 @@
-import Image from "next/image";
-import camart from '../assets/images/keith-camart.png'
+import camart from "../assets/images/keith-camart.png";
+import linked_in from "../assets/icons/linked_in.svg"
+import github from "../assets/icons/github.svg"
 
 export default function BlogAuthor() {
   return (
     <div className="blog-author-wrapper">
-      Keith Blackwood
-      <div className="blog-author-image">
-        <img src={camart.src}/>
+      <h1>About</h1>
+      <div className="blog-author-image-wrapper">
+        <img className="blog-author-image" src={camart.src} />
       </div>
+      <p>Keith is a full stack developer with a PhD in Tissue Engineering and experience in startup generation and technology transfer. Analytical thinking and a love of problem solving, he's happiest when he's halfway up a climbing project.</p>
       <div className="blog-social-media">
-        <div className="linkedin">LinkedIn</div>
-        <div className="github">GitHub</div>
+        <img className="linkedin-icon" src={linked_in.src}/>
+        <img className="github-icon" src={github.src}/>
       </div>
     </div>
   );
