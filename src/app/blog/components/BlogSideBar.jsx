444e281f66966eca7af86dea913f9d9d21c7f27e
Raxxius <raxxman@gmail.com>
2023-05-16 08:19:00 +0100

    added clarification comments

--- src/app/blog/components/BlogSideBar.jsx
+++ src/app/blog/components/BlogSideBar.jsx
@@ -11,10 +11,12 @@ export default function BlogSideBar(props) {
   for (let blog in data) {
     const date = data[blog].fileDate.toString().slice(0, 10);
     const title = data[blog].title;
-    const fileId = data[blog].fileId
+    const fileId = data[blog].fileId;
     let returnValue = (
-      <Link href={`/blog/${encodeURIComponent(fileId)}`}
-      className="sidebar-wrapper">
+      <Link
+        href={`/blog/${encodeURIComponent(fileId)}`}
+        className="sidebar-wrapper"
+      >
         <p className="sidebar-title">{title}</p>
         <p className="sidebar-date">{date}</p>
       </Link>
@@ -22,5 +24,12 @@ export default function BlogSideBar(props) {
     blogs.push(returnValue);
   }
 
-  return <div className="blog-sidebar">{blogs}</div>;
+  return (
+    <div className="blog-sidebar">
+      <div className="blog-sidebar-header">
+        <h1>click a link to head to the relevant blog</h1>
+      </div>
+      <div>{blogs}</div>;
+    </div>
+  );
 }
