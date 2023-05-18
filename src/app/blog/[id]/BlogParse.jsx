748adedfa387f71179a9545f88dfd09a748ef396
Raxxius <raxxman@gmail.com>
2023-05-18 10:21:08 +0100

    add key to blogParse

--- src/app/blog/[id]/BlogParse.jsx
+++ src/app/blog/[id]/BlogParse.jsx
@@ -1,91 +1,95 @@
-import React from "react"
+import React from "react";
 
 export default function BlogParse(item) {
-    const parseTag = (style) => {
-        return (
-        style === "HEADING_1" 
-        ?
-        "h1"
-        :
-        style === "HEADING_2"
-        ?
-        "h2"
-        :
-        style === "HEADING_3"
-        ?
-        "h3"
-        :
-        style === "SUBTITLE"
-        ?
-        "legend"
-        :
-        "p"
-        )
-    };
+  const parseTag = (style) => {
+    return style === "HEADING_1"
+      ? "h1"
+      : style === "HEADING_2"
+      ? "h2"
+      : style === "HEADING_3"
+      ? "h3"
+      : style === "SUBTITLE"
+      ? "legend"
+      : "p";
+  };
 
-    const parseElement = (num, tag) => {
-        // image
-        if (tag === "blog-solo-image") {
-            return (<img className="blog-solo-image" src={item.content[num].imageCuri} alt={item.content[num].imageDescription} referrerPolicy="no-referrer"/>)
-        }
-        if (item.className[num] === "blog-image") {
-            return (<img className="blog-inline-image" src={item.content[num].imageCuri} alt={item.content[num].imageDescription} referrerPolicy="no-referrer"/>)
-        }
-        // link
-        if (item.className[num] === "link") {
-            return (<a className="blog-link" href={item.content[num].link} target="_blank">{item.content[num].text}</a>)
-        }
-
-        item.className[num] === "bold"
-        ?
-        tag = "strong"
-        :
-        item.className[num] === "italics"
-        ?
-        tag = "i"
-        :
-        tag = tag
-
-        let className = item.className[num]
-
-        // specific coding to add legend className to picture legends.
-        if (tag === "legend") {
-            className = tag
-            tag = "p"
-        }
-
-        // generic return element h1, h2, h3, etc
-        return (React.createElement(
-            tag,
-            {className: className},
-            item.content[num]
-        ))
+  const parseElement = (num, tag, key) => {
+    // image
+    if (tag === "blog-solo-image") {
+      return (
+        <img
+          className="blog-solo-image"
+          src={item.content[num].imageCuri}
+          alt={item.content[num].imageDescription}
+          referrerPolicy="no-referrer"
+          key={key}
+        />
+      );
+    }
+    if (item.className[num] === "blog-image") {
+      return (
+        <img
+          className="blog-inline-image"
+          src={item.content[num].imageCuri}
+          alt={item.content[num].imageDescription}
+          referrerPolicy="no-referrer"
+          key={key}
+        />
+      );
+    }
+    // link
+    if (item.className[num] === "link") {
+      return (
+        <a className="blog-link" href={item.content[num].link} target="_blank" key={key}>
+          {item.content[num].text}
+        </a>
+      );
     }
 
+    item.className[num] === "bold"
+      ? (tag = "strong")
+      : item.className[num] === "italics"
+      ? (tag = "i")
+      : (tag = tag);
 
-    // add single component element
-    if (item.className.length === 1) {
-        let tag = parseTag(item.style)
-        if (item.className[0] === "blog-image") {
-            tag = "blog-solo-image"
-        }
-        return parseElement(0, tag)
-    };
+    let className = item.className[num];
 
-    const contentList = []
-
-    for (let i = 0; i < item.className.length; i++) {
-        let tag = "span"
-        contentList.push(parseElement(i, tag))
+    // specific coding to add legend className to picture legends.
+    if (tag === "legend") {
+      className = tag;
+      tag = "p";
     }
 
-    // add multi component element 
+    // generic return element h1, h2, h3, etc
+    return React.createElement(
+      tag,
+      { className: className, key: key},
+      item.content[num]
+    );
+  };
 
+  // add single component element
+  if (item.className.length === 1) {
     let tag = parseTag(item.style);
-    return (React.createElement (
-        tag,
-        {className: "blog-multi-element-container"},
-        contentList
-    ))
-}
+    if (item.className[0] === "blog-image") {
+      tag = "blog-solo-image";
+    }
+    return parseElement(0, tag);
+  }
 
+  const contentList = [];
+
+  for (let i = 0; i < item.className.length; i++) {
+    let tag = "span";
+    contentList.push(parseElement(i, tag, i));
+  }
+
+  // add multi component element
+
+  let tag = parseTag(item.style);
+  return React.createElement(
+    tag,
+    { className: "blog-multi-element-container" },
+    contentList
+  );
+}
