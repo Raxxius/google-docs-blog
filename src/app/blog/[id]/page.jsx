706d4903221c52cac01343d5e6bd2859f65b40d9
Raxxius <raxxman@gmail.com>
2023-07-09 15:56:44 +0100

    updated fetch request

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -9,7 +9,7 @@ import { use } from "react";
 export default function Page (props) {
   // datafetch
   const url =
-    "https://script.google.com/macros/s/AKfycbyOmNJ4J0EOouG61xVDeyC74Rr1W8TaT8sCGxcjCWZnpTLNCd0vipzPXT4pqdkCNfhs/exec";
+    "https://script.google.com/macros/s/AKfycbxKMnx_bsHRSdIWtQ4R_WlNwiK2ZVQEYMxuICMinFQvmwtkDJ-Csp9zTorjMc_kR6apbw/exec";
   const fileId = props.params.id;
   const fullUrl = `${url}?id=${fileId}`;
   const data = use(googleWebFetch(fullUrl));
