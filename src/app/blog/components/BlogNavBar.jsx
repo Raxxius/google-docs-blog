05282e666a8ac766f2d958ea0eaae04ace92542b
Raxxius <raxxman@gmail.com>
2023-07-25 16:23:37 +0100

    updated Navbar

--- src/app/blog/components/BlogNavBar.jsx
+++ src/app/blog/components/BlogNavBar.jsx
@@ -1,42 +1,18 @@
-import Moon from "@/app/assets/icons/moon.jsx";
-import Sun from "@/app/assets/icons/sun.jsx";
 import "./BlogNavBar.css";
 
-const Navbar = (props) => {
+const Navbar = () => {
   const navbarstyle = {
-    backgroundColor: props.darkMode ? "black" : "lightgrey",
-    color: props.darkMode ? "lightcyan" : "black",
+    backgroundColor: "black", 
+    color: "lightcyan",
     transition: "background-color 2s, color 2s",
-  };
-
-  const DarkModeButton = (props) => {
-    return (
-      <button className="darkMode-btn" onClick={props.handleDarkmode}>
-        {props.darkMode ? (
-          <Sun
-            style={{
-              fill: "#fbac13",
-              animation: "fadeIn 2s",
-            }}
-          />
-        ) : (
-          <Moon style={{ fill: "#F5F3CE", animation: "fadeIn 2s" }} />
-        )}
-      </button>
-    );
   };
 
   return (
     <div className="navbar" style={navbarstyle}>
       <div className="links-list">
-        <a href="" className="link-item">Home</a>
+        <a href="https://www.keith-blackwood.com/blog" className="link-item">Home</a>
       </div>
       <div className="links-spacer"></div>
-      <DarkModeButton
-        darkMode={props.darkMode}
-        setDarkMode={props.setDarkMode}
-        handleDarkmode={props.handleDar}
-      />
     </div>
   );
 };
