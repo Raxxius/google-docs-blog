efb0c34d719dd63a1db9d4f72aabd9ae8293b271
Raxxius <raxxman@gmail.com>
2023-07-25 16:36:26 +0100

    navbar styling update

--- src/app/blog/components/BlogNavBar.jsx
+++ src/app/blog/components/BlogNavBar.jsx
@@ -1,18 +1,13 @@
 import "./BlogNavBar.css";
 
 const Navbar = () => {
-  const navbarstyle = {
-    backgroundColor: "black", 
-    color: "lightcyan",
-    transition: "background-color 2s, color 2s",
-  };
 
   return (
-    <div className="navbar" style={navbarstyle}>
+    <div className="navbar">
       <div className="links-list">
-        <a href="https://www.keith-blackwood.com/blog" className="link-item">Home</a>
+        <a href="https://www.keith-blackwood.com/blog" className="link-item">Blog</a>
+        <a href="https://www.keith-blackwood.com/" className="link-item">Home</a>
       </div>
-      <div className="links-spacer"></div>
     </div>
   );
 };
