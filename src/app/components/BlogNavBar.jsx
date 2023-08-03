2a4efb361264f50cf848d8d1b48584f01d051233
Raxxius <raxxman@gmail.com>
2023-08-03 15:15:06 +0100

    updated anchor links

--- src/app/components/BlogNavBar.jsx
+++ src/app/components/BlogNavBar.jsx
@@ -5,8 +5,8 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="links-list">
-        <a href="https://www.keith-blackwood.com/blog" className="link-item">Blog</a>
-        <a href="https://www.keith-blackwood.com/" className="link-item">Home</a>
+        <a href="https://www.keith-blackwood.info/blog" className="link-item">Blog</a>
+        <a href="https://www.keith-blackwood.info/" className="link-item">Home</a>
       </div>
     </div>
   );
