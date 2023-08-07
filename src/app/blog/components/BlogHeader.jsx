6a754b3f5b873b76666da78277d52a12af84688f
Raxxius <raxxman@gmail.com>
2023-08-07 11:46:52 +0100

    removed redundant component folder

--- src/app/blog/components/BlogHeader.jsx
+++
@@ -1,9 +0,0 @@
-export default function BlogHeader() {
-
-    return (
-    <div className="blog-header">
-        <h1 className="blog-header-title">Coffee and Chat</h1>
-        <h2 className="blog-header-title">A blog (mostly) about coding</h2>
-    </div>
-    )
-}
