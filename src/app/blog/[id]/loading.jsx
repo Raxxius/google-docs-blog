bb05ae4947dba4dd62e854c91623338d8894b0c4
Raxxius <raxxman@gmail.com>
2023-08-07 12:04:40 +0100

    fixed broken dependancies

--- src/app/blog/[id]/loading.jsx
+++ src/app/blog/[id]/loading.jsx
@@ -1,7 +1,7 @@
-import BlogAuthor from "@src/app/components/BlogAuthor.jsx";
-import BlogHeader from "@src/app/components/BlogHeader.jsx";
-import Loader from "@src/app/components/Loader.jsx"
-import Navbar from "@src/app/components/BlogNavBar.jsx"
+import BlogAuthor from "src/app/components/BlogAuthor.jsx";
+import BlogHeader from "src/app/components/BlogHeader.jsx";
+import Loader from "src/app/components/Loader.jsx"
+import Navbar from "src/app/components/BlogNavBar.jsx"
 import "../blog.css";
 
 export default function LoadingPage() {
