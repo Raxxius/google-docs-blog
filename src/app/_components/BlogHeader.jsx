b2685c2eb45434036f11391d3781ce43195ec03e
Raxxius <raxxman@gmail.com>
2024-10-16 12:27:53 +0100

    altered hero image for faster loadtimes

--- src/app/_components/BlogHeader.jsx
+++ src/app/_components/BlogHeader.jsx
@@ -1,9 +1,25 @@
-export default function BlogHeader() {
+import Image from "next/image";
 
-    return (
+export default function BlogHeader() {
+  return (
     <div className="blog-header">
+      <Image
+        src="/blog/images/alexandru-acea-XEB8y0nRRP4-unsplash.jpg"
+        alt="Blog header image"
+        width={1200}
+        height={600}
+        layout="responsive"
+        priority
+        style={{ zIndex: 0, position: "absolute" }}
+      />
+      <div
+        style={{
+          zIndex: 1,
+        }}
+      >
         <h1 className="blog-header-title">Coffee and Chat</h1>
         <h2 className="blog-header-title">A blog (mostly) about coding</h2>
+      </div>
     </div>
-    )
+  );
 }
